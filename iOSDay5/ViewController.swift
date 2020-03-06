8851796159e9a4fdeccc53acc343929b5d7371d1
jhay <jhasondeleon@yahoo.com>
2020-03-06 18:12:59 -0500

    Initial Commit

---
+++ iOSDay5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDay5
+//
+//  Created by MacStudent on 2020-03-06.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
